9b882133f3933782c3284db702369a9a665989a4
rechatin <hugo.rechatin@eurecom.fr>
2022-04-25 14:14:53 +0200

    Fixed issue with some characters being more than 28 pixels wide

--- spritesectioning.py
+++ spritesectioning.py
@@ -28,13 +28,17 @@ def width_extract(image, dest_folder, i):
                 w += 1
             pixel_person_width = w - first_pixel
             null_width = 28 - pixel_person_width
-            if (pixel_person_width%2 == 0):
-                sample[:,:,:] = image[:,first_pixel-np.uint8(null_width/2) : (w)+np.uint8(null_width/2),:]
-            else:
-                sample[:,:,:] = image[:,first_pixel-np.uint8(np.ceil(null_width/2)) : (w)+np.uint8(np.floor(null_width/2)),:]
+            if (pixel_person_width < 28):
+                nw_ceil, nw_floor = np.uint8(np.ceil(null_width/2)) , np.uint8(np.floor(null_width/2))
+                sample[:,0:nw_ceil,:] = np.zeros((28,nw_ceil,4))
+                sample[:,nw_ceil:nw_ceil+pixel_person_width,:] = image[:,first_pixel:w,:]
+                sample[:,nw_ceil+pixel_person_width:28,:] = np.zeros((28,nw_floor,4))
+            elif(pixel_person_width>=28):
+                ew_ceil = np.uint8(np.ceil(-null_width/2))
+                ew_floor = np.uint8(np.floor(-null_width/2))
+                sample[:,:,:] = image[:,first_pixel+ew_ceil:w-ew_floor,:]
             cv2.imwrite(os.path.join(dest_folder, f"{str(i)}.png"), sample)
             i += 1
-            print(i)
             w += 1
         else:
             w += 1
@@ -53,9 +57,8 @@ def height_extract(image):
             h+=1
     return lines
 
-def extract_samples_from_images(og_folder,dest_folder):
+def extract_samples_from_images(og_folder,dest_folder, i):
     images = load_images_from_folder(og_folder)
-    i = 0
     lines = []
     for image in images:
         lines = height_extract(image)
@@ -63,8 +66,9 @@ def extract_samples_from_images(og_folder,dest_folder):
         for line in lines:
             print(line[0], line[1])
             i = width_extract(image[line[0]:line[1],:,:], dest_folder, i)
+            print (i)
 
 if __name__ == "__main__":
-    og_folder = 'Bros/Prepreprocess/images'
+    og_folder = 'Bros/Prepreprocess/images/unprocessed'
     dest_folder = 'Bros/Prepreprocess/samples'
-    extract_samples_from_images(og_folder,dest_folder)
+    extract_samples_from_images(og_folder,dest_folder, 2142)
